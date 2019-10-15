cd36a0072c7d11879345ce79d05161aee0c8c2f0
josed126 <clavellino1993@gmail.com>
2019-10-15 22:09:38 +0200

    initial

---
+++ AllEqual.java
@@ -0,0 +1,20 @@
+// Example of if statement use : 3-sort
+// Reads three integers from the command line, then prints them out in numerical order.
+
+public class AllEqual
+{
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+	int c = Integer.parseInt(args[2]);
+
+	if (a == b || a == c || c == b) {
+		 System.out.println("es igual");}
+	else {
+		 System.out.println("no es igual");
+}
+
+}  
+    
+}
