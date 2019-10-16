a3192b91874e55b03701ffab47904165d30dab81
josed126 <43546139+josed126@users.noreply.github.com>
2019-10-16 18:14:42 +0200

    Update AllEqual.java

--- AllEqual.java
+++ AllEqual.java
@@ -9,7 +9,7 @@ public class AllEqual
         int b = Integer.parseInt(args[1]);
 	int c = Integer.parseInt(args[2]);
 
-	if (a == b && a == c && c == b) {
+	if (a == b && a == c && b == c) {
 		 System.out.println("equal");}
 	else {
 		 System.out.println("not equal");
